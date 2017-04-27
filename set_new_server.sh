4406051411a5e77fb1d4292b04f0190136805239
Michał Zadrużyński <michal@on-running.com>
2017-04-27 11:15:22 +0200

    Revert "add change root passowrd and create ssh keys" this was bad idea This reverts commit 447f6a54fb29e504ebbc5d0efd53d96eb13b7acf.

--- set_new_server.sh
+++ set_new_server.sh
@@ -1,9 +1,5 @@
 echo "setting new server..."
 
-sudo su
-echo "first step - change root password"
-passwd
-
 printf "\n"
 printf "updating apt-get\n"
 apt-get update
@@ -22,10 +18,5 @@ sh scripts/set_git.sh
 printf "\n"
 sh scripts/set_vim.sh
 printf "\n"
-echo "generating ssh keys"
-ssh-keygen -q -N "" -f "$HOME/.ssh/id_rsa"
-echo "use this public key for git repo"
-echo ""
-cat ~/.ssh/id_rsa.pub
-echo ""
+
 echo "server up and ready!"
